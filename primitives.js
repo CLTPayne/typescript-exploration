10f64875e3aebc39ffdb7093af45b1d31f467843
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-10-03 22:50:36 +0100

    Add javascript primitives

---
+++ primitives.js
@@ -0,0 +1,15 @@
+typeof true // 'boolean'
+
+typeof null // 'object'
+
+typeof undefined // 'undefined'
+
+typeof Infinity // 'number'
+
+typeof 10.1 // 'number'
+
+typeof 'z' // 'string'
+
+typeof Symbol('z') // 'symbol'
+
+console.log(Symbol('z') === Symbol('z')) // 'false'
